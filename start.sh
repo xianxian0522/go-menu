6e7e58beadfdad173bab1b0479b2431c720713b4
zixuan.wang <zixuan.wang@sumscope.com>
2021-07-16 10:20:46 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -5,8 +5,7 @@ originalDir=`pwd`
 bashFileDir="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
 serviceDir="$( cd "${bashFileDir}/bin" && pwd )"
 
-echo "生成配置文件"
-sh exportenv.sh $ENV
+
 echo "启动内容"
 
 cname=$start_name
